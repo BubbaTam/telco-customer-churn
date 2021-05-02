3ae65d194e07e81ec4bda18c43275aabe062d256
Bubba Tam <jamesglassey@outlook.com>
2021-05-02 08:07:46 +0100

    New functions (standardise, normalise, identifying input features

--- src/dataset.py
+++ src/dataset.py
@@ -20,13 +20,12 @@ pd.set_option("display.max_columns",60)
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
 from sklearn.compose import ColumnTransformer
 #local application/library specific imports
-#import src.utils as utils
-#%%
-os.chdir(r'C:\Users\james\OneDrive\Desktop\Data Science\modelling\Churn prediction\Telco customer churn')
-print(os.getcwd())
-
+import src.utils as utils
+import src.dispatcher as dispatcher
 
 #%%
 def sorting_categorical_features(df,binary_features,
@@ -46,35 +45,40 @@ def sorting_categorical_features(df,binary_features,
 #%%
 df = pd.read_csv('data/telco-customer-churn-clean-folds.csv')
 #%%
-binary_features = ['gender','senior_citizen','partner','dependents','phone_service',
-                   'paperless_billing']
-nominal_features = ['multiple_lines', 'internet_service','online_security',
-                    'online_backup','device_protection','tech_support',
-                    'streaming_tv','streaming_movies','contract',
-                    'payment_method'
-                    ]
 
-numerical_features = ['tenure','monthly_charges','total_charges']
-target_feature =['churn']
-fold_feature = ['k_fold']
-#sorting_categorical_features(df,binary_features,nominal_features)
 
 #%%
-def sorted_categorical_features(dataframe,
-                                binary_features = None,
-                                ordinal_features = None,
-                                ordinal_features_order = 'auto',
-                                nominal_features = None,
-                                target_feature=None):
+def sorted_categorical_features_df(dataframe,
+                                   binary_features = None,
+                                   ordinal_features = None,
+                                   ordinal_features_order = 'auto',
+                                   numerical_features = None,
+                                   nominal_features = None,
+                                   target_feature=None,
+                                   fold_feature = None
+                                   ):
     """
     prior:
-        - the data is cleared
+        - the data is cleaned
+        - no missing data
+    points of contention:
+        - the use of labelencoder so will just use ordinalencoder
+    options:
+        - create a new dataframe with all featues 
+        - to return the input and target feature/s separately 
     """
     
     df_2 = dataframe.copy()
     # Encode the target feature
-    df_2.loc[:,target_feature] = LabelEncoder().fit_transform(df_2[target_feature].values)
-    #df_2.loc[:,target_feature] = df_2.loc[:,target_feature].values.ravel()
+    enc = OrdinalEncoder(dtype = int)
+    df_2.loc[:,target_feature] = enc.fit_transform(df_2.loc[:,target_feature])
+    
+    
+    ###if i was to split the data from here into input and target variable/s
+    #df_2.loc[:,target_feature] = LabelEncoder().fit_transform(df_2[target_feature].values)
+    
+    
+    
     # Encode binary input features 
     if binary_features != None:
         for column in binary_features:
@@ -87,8 +91,8 @@ def sorted_categorical_features(dataframe,
                               dtype = int
                               )
         df_2[ordinal_features] = enc.fit_transform(df_2[ordinal_features])
-        df_2[ordinal_features] = enc.fit_transform(df_2[ordinal_features])  
-    
+        df_2[ordinal_features] = enc.fit_transform(df_2[ordinal_features])
+        
                    
     # Encode nominal input features 
     if nominal_features != None:
@@ -111,30 +115,24 @@ def sorted_categorical_features(dataframe,
                               axis=1)
     
     return df_sorted_cf
-    
-    
+
+def standardise_numerical_features(df,numerical_features):
+    df[numerical_features] = StandardScaler().fit_transform(df[numerical_features])
+def normalise_numerical_features(df,numerical_features):
+    df[numerical_features] = Normalizer().fit_transform(df[numerical_features])
+def identify_input_features(binary_features,
+                            ordinal_features,
+                            numerical_features,
+                            nominal_features,
+                            target_feature,
+                            fold_feature):
+    """
+    Default is that target_feature and fold_feature are not included.
+    The function identifies datatypes that are not present and excludes them  
+    """
+    not_input_features = [target_feature,fold_feature]
+    [not_input_features.append(i) for i in dispatcher.features if dispatcher.features[i] == None]
+    input_features = [i for i in dispatcher.features if i not in not_input_features] 
+    return input_features
+     
    
-
-
-
-#%%
-df_sorted_cf = sorted_categorical_features(df,
-                                           binary_features = binary_features,
-                                           ordinal_features = None,
-                                           ordinal_features_order = 'auto',
-                                           nominal_features = nominal_features,
-                                           target_feature = target_feature)
-  #%%
-print(sorted(df_sorted_cf.columns))
-print('***')
-print(sorted(df.columns))
-#%%
-pete = df[binary_features+
-          nominal_features+
-          numerical_features+
-          target_feature+
-          fold_feature]
-
-for f in df.columns:
-    if f not in pete.columns:
-        print(f )
