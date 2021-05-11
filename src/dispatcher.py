8dacebb80f5f02f76e59c6380da36e3b63b640b3
Bubba Tam <jamesglassey@outlook.com>
2021-05-11 09:54:24 +0100

    updating parameters-XGB... value to a list from a tuple

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -47,7 +47,7 @@ parameters = {
     'XGBoost_classifier' :
         {
         'eta':[0.01,0.015,0.025,0.05,0.1],
-        'gamma':(0.05,0.1,0.3,0.5,0.7,0.9,1.0),
+        'gamma':[0.05,0.1,0.3,0.5,0.7,0.9,1.0],
         'max_depth':[3,5,7,9,12,15,17,25],
         'min_child_weight': [1,3,5,7],
         'subsample': [0.6,0.7,0.8,0.9,1.0],
@@ -55,30 +55,36 @@ parameters = {
         'lambda': [0.01,0.1,1.0],
         'alpha': [0,0.1,0.5,1.0]
         },
-    'logistic_regression' :
+    'logistic_regression' :                    
         {
         'C':[0.01,0.05,0.1,0.5,1.0,10]
         },
-    'rf' :
+    # possible parameters for logistic regression with sklearn:
+        # penalty{‘l1’, ‘l2’, ‘elasticnet’, ‘none’}, default=’l2’
+        # C: float, default=1.0
+        
+    'rf' : # fine
         {
         'n_estimators':[120,300,500,800,1200],
-        'max_depth':[5,8,15,25,30,None],
-        'min_samples_split': [1,2,5,10,15,100],
-        'min_samples_leaf': [1,2,5,10],
-        'max features': ['auto','log2','sqrt',None]
+        'max_depth':[5,8,15,25,30],
+        'min_samples_split': [2,5,10,15,100],  
+        'min_samples_leaf': [1,2,5,10]
         },
     'decision_tree' :
         {
-            'criterion': ['gini','entropy'],
-            'max_depth':[5,8,15,25,30,None],
-            'min_samples_split': [1,2,5,10,15,100],
-            'min_samples_leaf': [1,2,5,10],
-            'max features': ['auto','log2','sqrt',None]
+        'criterion': ['gini','entropy'],
+        'max_depth':[5,8,15,25,30],
+        'min_samples_split': [2,5,10,15,100],
+        'min_samples_leaf': [1,2,5,10]
         },
-    'SVC' :
+    'SVM' :
         {
-        'C':[0.001,0.01,0.1,10,100,1000],
-        'gamma':['gamma','auto'],
-        'class_weight': ['balanced',None]}
+        'kernel':['poly','rbf', 'sigmoid'],
+        'C':[0.001,0.01,0.1,1,10,100,1000],
+        'gamma':[0.0001,0.001,0.01,0.1,1]
+        }
                 }
 ordinal_feature_order = None
+#%%
+print(tree.DecisionTreeClassifier().get_params().keys())
+#        'max features': ['auto','log2','sqrt']
