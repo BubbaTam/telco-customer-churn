6d363db7d993ed04556e1fcf9155b0868ca7780c
Bubba Tam <jamesglassey@outlook.com>
2021-05-13 19:26:29 +0100

    start of making a function for feature selection

--- src/feature_generator.py
+++ src/feature_generator.py
@@ -32,9 +32,28 @@ def numerical_feature_selection(dataframe,
     holder = VarianceThreshold(threshold=threshold * (1-threshold))
     dataframe[numerical_features] = holder.fit_transform(dataframe)
     
+    
+def feature_selection(dataframe,
+                      feature_selection_correlation,
+                      ordinal_features = None,
+                      nominal_features = None,
+                      numerical_features = None,
+                      target_feature=None):
+    if feature_selection_correlation == "Pearson":
+        
+    elif feature_selection_correlation == 'Spearman':
+        
+    elif feature_selection_correlation == 'ANOVA':
+        
+    elif feature_selection_correlation == 'Kendall':
+        
+    elif feature_selection_correlation == 'Chi-squared':
+    
 # def feature_selection(dataframe):
 
 #%%
 # binary_feature_selection(df,
 #                          binary_features = b_f,
 #                          threshold=)
+
+
